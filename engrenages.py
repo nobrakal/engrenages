fc71fb1a75c97af3e73414fb0e1ed619435d1b78
Alexandre Moine <nobrakal@gmail.com>
2016-01-17 14:56:03 +0100

    Support de plusieurs connections

--- engrenages.py
+++ engrenages.py
@@ -5,19 +5,49 @@ A simple echo server
 """ 
 
 import socket 
+import select
+import ssl
 
 host = '' 
 port = 80 
-backlog = 5 
+backlog = 10 # Nombre de connections maximum
 size = 1024 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+socket_list = []
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host,port)) 
 s.listen(backlog) 
-client, address = s.accept() 
+
+socket_list.append(s)
 while 1: 
-	data = client.recv(size) 
-	if data.decode() == "exit\r\n":
-		client.close()
-		break
-	elif data: 
-		client.send(data)
+	# prend une liste des socket prêt à être lu.
+	ready_to_read,ready_to_write,in_error = select.select(socket_list,[],[])
+
+	for sock in ready_to_read:
+		# Une nouvelle connection!
+		if sock == s: 
+			newsocket, addr = s.accept()
+			socket_list.append(newsocket)
+			print ("Client connecté")
+
+		# Un message d'un client existant
+		else: 
+			try:
+			# Reception des données...
+				data = sock.recv(size)
+				if data:
+                        	# Des données sont arrivées
+					print(data) 
+				else:
+				# Il n'y a rien, le client est sans doute déconnecté
+					if sock in socket_list:
+						socket_list.remove(sock)
+					print("Client perdu")
+
+			# exception 
+			except:
+				if sock in socket_list:
+					socket_list.remove(sock)
+				print("Client perdu")
+				continue
+s.close()
