96903b069231c7c1ec56ac69a1e632b5a493d289
Alexandre Moine <nobrakal@gmail.com>
2016-01-24 11:21:21 +0100

    Le client se connecte au serveur, même à distance!

--- engrenages.py
+++ engrenages.py
@@ -12,16 +12,18 @@ import pickle
 import time
 
 port_serveur = 6666
-port_client = 81
+port_client = 6667
 backlog = 10 # Nombre de connections maximum
 size = 1024 
+
+id_list = []
 
 def sendTimedMessage(msg, socket_list):
 	"""Envoi un message avec son id, le temps, à la liste de socke"""
 	for sock in socket_list[1:]:
 		time = datetime.datetime.now()
 		id_list.append(time) # Ajoute notre propre message à la liste des messages envoyés
-		sock.send(pickle.dumps((str(time)+self.msg))) # Envoi le temps afin d'éviter les boucles d'envoi infinies (=id du message), plus le message
+		sock.send(pickle.dumps((str(time)+msg))) # Envoi le temps afin d'éviter les boucles d'envoi infinies (=id du message), plus le message
 
 def sendMessage(msg, socket_list):
 	"""Envoi un message à la liste de socket"""
@@ -33,10 +35,10 @@ class Serveur():
 
 	def __init__(self):
 		self.socket_list = []
-		self.id_list = []
 		self.active_sock_list = []
 
 		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		self.s.bind(('',port_serveur)) 
 		self.s.listen(backlog) 
 
@@ -56,7 +58,7 @@ class Serveur():
 					self.newsocket, self.addr = self.s.accept()
 					print ("Client connecté")
 
-					if len(self.socket_list) != 1: # Cas où il ne reste plus qu'un seul socket, le notre, on n'envoi donc aucune adresse
+					if len(self.socket_list) != 1: # Cas où il ne reste plus qu'un seul socket, le notre, on n'envoit donc aucune adresse
 						for self.active_sock in self.socket_list[1:]: # On envoi la liste des ip des connectés, sauf la notre
 							self.active_sock_list.append(self.active_sock.getpeername())					
 						self.newsocket.send(pickle.dumps(self.active_sock_list))
@@ -70,9 +72,9 @@ class Serveur():
 					if self.data:
        		                	# Des données sont arrivées
 						self.data = pickle.loads(self.data)
-						if self.data[:26] not in self.id_list: # Les 26 premiers charactères correspondent à la date
+						if self.data[:26] not in id_list: # Les 26 premiers charactères correspondent à la date
 								print(self.data[26:])
-								self.id_list.append(self.data[:26]) # Ajoute l'id du message, il ne sera pas rééaffiché en cas de nouvelle récéption
+								id_list.append(self.data[:26]) # Ajoute l'id du message, il ne sera pas rééaffiché en cas de nouvelle récéption
 								sendMessage(self.data, self.socket_list) # Renvoi le message aux autres serveurs, afin d'assurer une propagation optimale
 
 					else:
@@ -93,11 +95,10 @@ class Client():
 
 	def __init__(self):
 		self.socket_list = []
-		self.id_list = []
 
-		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-		self.socket_list.append(self.s)
+		self.socket_list.append(self.c)
 
 		self.thread = threading.Thread(target=self.startClient)
 		self.thread.start()     
@@ -105,10 +106,10 @@ class Client():
 	def startClient(self):
 		"""Code à exécuter pendant l'exécution du client."""
 		#On se connecte au serveur local
-		self.ConnectNewServer('',6666)
+		self.ConnectNewServer('')
 		while 1:
 			# prend une liste des socket prêt à être lu.
-			self.ready_to_read,self.ready_to_write,self.in_error = select.select(self.socket_list,[],[])
+			self.ready_to_read,self.ready_to_write,self.in_error = select.select([],self.socket_list,[])
 			for self.sock in self.ready_to_read:
 				# Un message d'un client existant
 				# Reception des données...
@@ -120,11 +121,11 @@ class Client():
 						print("Liste d'ip arrivées:" +str(pickle.loads(self.data)))
 						self.ip_list = pickle.loads(self.data)
 						for self.ip2connect in self.sock_list: # On s'y connecte
-							self.s.connect((ip2connect,port_serveur))
+							self.c.connect((ip2connect,port_serveur))
 						
-					elif self.data[:26] not in self.id_list: # Les 26 premiers charactères correspondent à la date
+					elif self.data[:26] not in id_list: # Les 26 premiers charactères correspondent à la date
 							print(self.data[26:])
-							self.id_list.append(self.data[:26]) # Ajoute l'id du message, il ne sera pas rééaffiché en cas de nouvelle récéption
+							id_list.append(self.data[:26]) # Ajoute l'id du message, il ne sera pas rééaffiché en cas de nouvelle récéption
 							sendMessage(self.data, self.socket_list) # Renvoi le message aux autres serveurs, afin d'assurer une propagation optimale
 
 				else:
@@ -135,13 +136,22 @@ class Client():
 	
 			if len(self.socket_list) == 1: # Cas où il ne reste plus qu'un seul socket, le notre.
 				print("Fermeture, plus aucun client connecté")
-				self.s.close()
+				self.c.close()
 				return 0
 
-	def ConnectNewServer(self, ip, port=port_client):
+	def ConnectNewServer(self, ip, port=port_serveur):
 		self.ip = ip
+		self.port = port
+
+		# Création d'un socket pour la nouvelle connection
+
+		ysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 		try:
-			self.s.connect((self.ip,port_client))# On se connecte au nouveau serveur.
+			ysock.connect((self.ip,port))# On se connecte au nouveau serveur.
+			self.socket_list.append(ysock)
+			if ip != '':
+				print("Connecté au serveur distant d'ip "+str(ip))
 		except Exception as e: 
 			print("Quelque chose c'est mal passé avec %s:%d. l'exception est %s" % (self.ip,port_serveur, e))
 
@@ -151,6 +161,7 @@ serveur = Serveur()
 time.sleep(2)
 client = Client()
 time.sleep(2)
+
 client.ConnectNewServer("78.205.80.129")
 msg = input("Entrez votre message : ")
 sendTimedMessage(msg,serveur.getSocketList())
