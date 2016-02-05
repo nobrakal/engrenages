48b54ff596711b6d98c0642319ea9f10f6e85158
Alexandre Moine <nobrakal@gmail.com>
2016-02-05 19:56:46 +0100

    Simplifications

--- engrenages.py
+++ engrenages.py
@@ -17,18 +17,16 @@ port_client = 6667
 backlog = 10 # Nombre de connections maximum
 size = 1024 
 
-id_list = []
-
-def sendTimedMessage(msg, socket_list):
-	"""Envoi un message avec son id, le temps, à la liste de socke"""
+def sendTimedMessage(msg, socket_list,client):
+	"""Envoi un message avec son id, le temps, à la liste de socket. Ajoute l'id à la liste."""
 	for sock in socket_list[1:]:
 		time = datetime.datetime.now()
-		id_list.append(time) # Ajoute notre propre message à la liste des messages envoyés
+		client.id_list.append(time) # Ajoute notre propre message à la liste des messages envoyés
 		sock.send(pickle.dumps((str(time)+msg))) # Envoi le temps afin d'éviter les boucles d'envoi infinies (=id du message), plus le message
 
 def sendMessage(msg, socket_list):
 	"""Envoi un message à la liste de socket"""
-	for sock in socket_list[1:]:
+	for sock in socket_list:
 		sock.send(pickle.dumps(msg)) # Envoi du message (réencodé).
 
 class Serveur():
@@ -46,11 +44,11 @@ class Serveur():
 		self.socket_list.append(self.s)
 
 		self.thread = threading.Thread(target=self.startServeur)
-		self.thread.start()     
+		self.thread.start()
 
 	def startServeur(self):
 		"""Code à exécuter pendant l'exécution du serveur."""
-		self.newsocket, self.addr = self.s.accept() # Attends la connection du client
+		self.newsocket, self.addr = self.s.accept() # Attends la connection du client local
 		self.socket_list.append(self.newsocket)
 		print("Client local connecté")
 		while 1:
@@ -63,10 +61,9 @@ class Serveur():
 					print ("Client connecté")
 
 					# On envoi l'adresse du client à notre client, pour qu'il puisse se connecter au serveur.
-					self.socket_list[1].send(pickle.dumps(self.newsocket.getpeername()))
+					sendMessage(self.newsocket.getpeername(), [self.socket_list[1]]) 
 
-
-					if len(self.socket_list) != 0: # Cas où il ne reste plus qu'un seul socket, le notre, on n'envoit donc aucune adresse
+					if len(self.socket_list) > 2: # Cas où il y a plus de deux socket (le serveur et le client local)
 						for self.active_sock in self.socket_list[1:]: # On envoi la liste des ip des connectés, sauf la notre
 							self.active_sock_list.append(self.active_sock.getpeername())					
 						self.newsocket.send(pickle.dumps(self.active_sock_list))
@@ -78,12 +75,7 @@ class Serveur():
 					# Reception des données...
 					self.data = self.sock.recv(size)
 					if self.data:
-       		                	# Des données sont arrivées
-						self.data = pickle.loads(self.data)
-						if self.data[:26] not in id_list: # Les 26 premiers charactères correspondent à la date
-								print(self.data[26:])
-								id_list.append(self.data[:26]) # Ajoute l'id du message, il ne sera pas rééaffiché en cas de nouvelle récéption
-								sendMessage(self.data, self.socket_list) # Renvoi le message aux autres serveurs, afin d'assurer une propagation optimale
+						sendMessage(self.data, [self.socket_list[1]]) # Envoi du message à notre client local
 
 					else:
 					# Il n'y a rien, le client est sans doute déconnecté
@@ -101,13 +93,14 @@ class Client():
 
 	def __init__(self, serveur):
 		self.socket_list = []
+		self.id_list=[]  
 
 		self.c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 		self.socket_list.append(self.c)
 
 		self.thread = threading.Thread(target=self.startClient)
-		self.thread.start()     
+		self.thread.start()   
 
 	def startClient(self):
 		"""Code à exécuter pendant l'exécution du client."""
@@ -132,10 +125,10 @@ class Client():
 					elif len(self.data) < 26: # Cas de reception d'une ip
 						self.ConnectNewServer(self.data) # On s'y connecte
 						
-					elif self.data[:26] not in id_list: # Les 26 premiers charactères correspondent à la date
+					elif self.data[:26] not in self.id_list: # Les 26 premiers charactères correspondent à la date
 							print(self.data[26:])
-							id_list.append(self.data[:26]) # Ajoute l'id du message, il ne sera pas rééaffiché en cas de nouvelle récéption
-							sendMessage(self.data, self.socket_list) # Renvoi le message aux autres serveurs, afin d'assurer une propagation optimale
+							self.id_list.append(self.data[:26]) # Ajoute l'id du message, il ne sera pas rééaffiché en cas de nouvelle récéption
+							sendMessage(self.data, self.socket_list[1:]) # Renvoi le message aux autres serveurs, afin d'assurer une propagation optimale
 
 				else:
 				# Il n'y a rien, le client est sans doute déconnecté
@@ -217,4 +210,4 @@ client = Client(serveur)
 
 #client.ConnectNewServer("78.205.80.129")
 #msg = input("Entrez votre message : ")
-#sendTimedMessage(msg,client.socket_list)
+#sendTimedMessage(msg,client.socket_list,client)
