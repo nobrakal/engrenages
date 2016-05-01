be84ae4a89f290d066a6de0b600dd78c4aab40e1
Alexandre Moine <nobrakal@gmail.com>
2016-05-01 15:09:00 +0200

    Améliore les arguments

--- engrenages.py
+++ engrenages.py
@@ -12,18 +12,15 @@ from graphicsCurses import *
 
 parser = argparse.ArgumentParser() # Prend en compte les arguments
 parser.add_argument("--pseudo", help="Lance engrengages avec un pseudo")
-parser.add_argument("--port", help="Lance engrengages sur le port précisé")
-parser.add_argument("--gui", help="Si 1, lance engrenages en mode graphique (DEFAUT), si 0 lance engrenages en mode text")
-parser.add_argument("--debug", help="Si 1, lance engrenages en mode debug, si 0 lance engrenages normalement (DEFAUT)")
+parser.add_argument("--port",default=6666, help="Lance engrengages sur le port précisé")
+parser.add_argument("--nogui",nargs="?",const=True,default=False, help="Si spécifié, lance engrenages en mode text")
+parser.add_argument("--debug",nargs="?",const=True,default=False, help="Si spécifié, lance engrenages en mode debug")
 args = parser.parse_args()
 
-if args.gui == None:
-	gui = True
-else:
+if args.nogui:
 	gui = False
-
-if args.port == None:
-	args.port = 6666
+else:
+	gui = True
 
 rouage = Rouage(int(args.port), 10, 1024)
 rouage.start()
@@ -46,8 +43,7 @@ else:
 rouage.pseudo = pseudo
 rouage.pseudo_list.append(pseudo)
 
-if args.debug != None:
-	rouage.debug = True
+rouage.debug = args.debug
 
 if gui == True and pseudo != "":
 	rouage.gui = True
